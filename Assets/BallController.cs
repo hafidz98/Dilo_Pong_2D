4df12dda3b6e8541fd8ee58fbfb205ccd5ccca02
Hafidz <public.acc78@gmail.com>
2021-10-27 22:11:05 +0700

    create and implement ball control

---
+++ Assets/BallController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    private Rigidbody2D rigidBody2D;
+
+    public float xInitialForce;
+    public float yInitialForce;
+
+    void ResetBall(){
+        transform.position = Vector2.zero;
+        rigidBody2D.velocity = Vector2.zero;
+    }
+
+    void PushBall(){
+        float yRandomInitialForce = Random.Range(-yInitialForce, yInitialForce);
+        float randomDirection = Random.Range(0, 2);
+
+        if (randomDirection < 1.0f){
+            rigidBody2D.AddForce(new Vector2(-xInitialForce, yRandomInitialForce));
+        } else {
+            rigidBody2D.AddForce(new Vector2(xInitialForce, yRandomInitialForce));
+        }
+    }
+
+    void RestartGame(){
+        ResetBall();
+        Invoke("PushBall", 2);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        RestartGame();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
